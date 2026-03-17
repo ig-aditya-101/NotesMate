e8642f635d1bd6ae26dfd237880443e105117870
ig-aditya-101 <adistuding@gmail.com>
2026-03-17 16:15:53 +0530

    button component updates

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 
-const Button = ({ variant='primary', children, ...props  }) => {
+const Button = ({ variant='primary', children, onClick }) => {
   const variants ={
-    primary:' bg-bg-inverse text-text-inverse border-border-main',
+    primary:' bg-bg-inverse text-text-inverse border-border-strong',
     secondary:"text-text-primary  border-border-strong",
     danger:"bg-error-bg text-text-inverse border-border-main",
   }
   return (
     <div className="">
-      <button className= {` text-label border-2 rounded-2xl px-6 py-3.5 radius-lg ${variants[variant]}`}>
+      <button onClick={onClick} className= {` text-label border-2 rounded-2xl px-6 py-3.5  ${variants[variant]}`}>
         {children}
       </button>
     </div>
