629c0c170dd216bc34c4727ea9bac9d3ad6b5c22
ig-aditya-101 <adistuding@gmail.com>
2026-03-19 16:31:51 +0530

    feat: setup pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,27 @@
-import Badge from "./utils/Badge";
-import Button from "./utils/Button";
-import FilterBar from "./components/FilterBar";
-import Input from "./utils/Input";
-import Modal from "./utils/Modal";
-import NotesCard from "./components/NotesCard";
-import SearchBar from "./components/SearchBar";
-import Toast from "./utils/Toast";
-import Toggle from "./utils/Toggle";
-import Navbar from "./components/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import BrowsePage from "./pages/BrowsePage";
+import ProfilePage from "./pages/ProfilePage";
+import dashboard from "./pages/dashboard";
+import RegisterPage from "./pages/registerPage";
+import LoginPage from "./pages/LoginPage";
+import ContributorsPage from "./pages/ContributorsPage";
+import UploadPage from "./pages/UploadPage";
+dashboard;
 
 const App = () => {
   return (
     <>
-      <div className="">
-     <NotesCard title='Complete Linear Algebra Notes' subject='Mathematics' name='Akanksha V.' downloads='123'/>
-     <Navbar/>
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<BrowsePage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/Contributors" element={<ContributorsPage />} />
+          <Route path="/upload" element={<UploadPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 };
