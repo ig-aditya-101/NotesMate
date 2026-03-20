cf98f8dd9412996a9cb4db650e6c3f4a5b14b991
ig-aditya-101 <adistuding@gmail.com>
2026-03-20 12:39:56 +0530

    added some features

--- client/src/utils/Input.jsx
+++ client/src/utils/Input.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Input = ({ type, placeholder, value, onChange, error }) => {
   return (
-    <div className=" ">
+    <div className="w-full ">
       <input
-        className="  border-border-main bg-bg-secondary text-text-muted rounded-xl text-body px-4 py-3"
+        className="  border-border-main border-3 bg-bg-primary text-text-primary placeholder:text-text-muted rounded-xl text-body px-4 py-3 outline-none w-full"
         type={type}
         placeholder={placeholder}
         value={value}
--- client/src/utils/Toggle.jsx
+++ client/src/utils/Toggle.jsx
@@ -1,19 +1,19 @@
-import React, { useState } from "react";
+;
 
-const Toggle = ({}) => {
-  const [isStudent, setIsStudent] = useState(false);
+const Toggle = ({ value, onChange }) => {
+  
 
   return (
-    <div className="flex gap-2.5  border-bg-secondary border-2 text-h3 rounded-xl w-fit overflow-clip">
+    <div className="flex gap-2.5  border-border-main border-3 text-h3 rounded-xl  overflow-clip w-full">
       <div
-      onClick={()=>setIsStudent(true)}
-        className={` px-10 py-1.5 cursor-pointer transition-all duration-200 ease-out ${isStudent ? "bg-text-secondary text-text-inverse  rounded-xl" : "bg-bg-primary text-text-secondary"}`}
+        onClick={() => onChange(true)}
+        className={`w-full flex justify-center  py-2 cursor-pointer transition-all duration-200 ease-out ${value ? "bg-text-secondary text-text-inverse  rounded-xl" : "bg-bg-primary text-text-secondary"}`}
       >
         Student
       </div>
       <div
-       onClick={()=>setIsStudent(false)}
-        className={` px-10 py-1.5 cursor-pointer transition-all duration-200 ease-out ${isStudent ? "bg-bg-primary text-text-secondary" : "bg-text-secondary text-text-inverse  rounded-xl"}`}
+        onClick={() => onChange(false)}
+        className={`w-full flex justify-center py-2 cursor-pointer transition-all duration-200 ease-out ${value ? "bg-bg-primary text-text-secondary" : "bg-text-secondary text-text-inverse  rounded-xl"}`}
       >
         Teacher
       </div>
