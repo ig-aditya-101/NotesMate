5728878ff587d9eeafee7bda14c7cb9352ca1b61
ig-aditya-101 <adistuding@gmail.com>
2026-03-28 18:12:01 +0530

    feat:college controller

---
+++ server/controllers/collegeController.js
@@ -0,0 +1,13 @@
+import College from "../models/college.js";
+
+export const searchColleges= async(req,res)=>{
+    try{
+        const search= req.query.search;
+        const colleges= await College.find({ name: { $regex:`^${search}`, $options: "i" } }).limit(20);
+        res.json(colleges);
+
+    }
+    catch(err){
+        res.status(500).json({message:err.message})
+    }
+}
