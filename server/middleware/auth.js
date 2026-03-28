f5c492387a45f09a0f751da24f8d977119a61686
ig-aditya-101 <adistuding@gmail.com>
2026-03-28 18:12:41 +0530

    feat authentication middleware

---
+++ server/middleware/auth.js
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+
+export const tokenVerification = async (req, res, next) => {
+  try {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+      return res.status(400).json({ message: "not authorised" });
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = await User.findById(decoded.id);
+    next();
+  } catch (error) {
+    res.status(401).json({ message: error.message });
+  }
+};
