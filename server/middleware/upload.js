d649063dcdf50c2a2d4baf334cce45464fd7f2d7
ig-aditya-101 <adistuding@gmail.com>
2026-04-01 12:36:45 +0530

    feat:upload function

---
+++ server/middleware/upload.js
@@ -0,0 +1,15 @@
+import multer from "multer";
+
+const storage = multer.memoryStorage();
+
+export const  uploadMiddleware = multer({
+  storage,
+  limits: { fileSize: 15 * 1024 * 1024 },
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype === "application/pdf") {
+      cb(null, true);
+    } else {
+      cb(null, false);
+    }
+  },
+});
