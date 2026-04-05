16e4207c1bb4a922f65c6b22b5d508380728a376
ig-aditya-101 <adistuding@gmail.com>
2026-04-05 10:39:27 +0530

    feat:added rate notes route

---
+++ server/routes/note.js
@@ -0,0 +1,27 @@
+import { Router } from "express";
+import { uploadMiddleware } from "../middleware/upload.js";
+import {
+  deleteNotes,
+  downloadNotes,
+  getNotes,
+  rateNotes,
+  trendingNotes,
+  uploadNotes,
+} from "../controllers/noteController.js";
+import { tokenVerification } from "../middleware/auth.js";
+const router = Router();
+
+router
+  .post(
+    "/upload",
+    tokenVerification,
+    uploadMiddleware.single("file"),
+    uploadNotes,
+  )
+  .get("/", tokenVerification, getNotes)
+  .get("/trending", tokenVerification, trendingNotes)
+  .get("/:id/download", tokenVerification, downloadNotes)
+  .delete("/:id", tokenVerification, deleteNotes)
+  .post("/:id/rate",tokenVerification, rateNotes);
+
+export default router;
