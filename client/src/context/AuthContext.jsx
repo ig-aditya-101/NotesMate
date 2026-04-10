8fc1d7f1219ba9a5a8018e85ab8988df04179e32
ig-aditya-101 <adistuding@gmail.com>
2026-04-11 02:39:27 +0530

    feat:updated authcontext with userb fetching

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,17 +1,37 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { createContext } from "react";
+import axiosInstance from "../apis/axios";
 
 export const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
-    const [token, setToken] = useState();
-    const [user, setUser] = useState();
+  const [token, setToken] = useState(
+    localStorage.getItem("NOTESMATE_TOKEN") || null,
+  );
+  const [user, setUser] = useState();
+  useEffect(() => {
+    const fetchUser = async () => {
+      if (token) {
+        try {
+          const res = await axiosInstance.get("/auth/me");
+          const user = res.data.user;
+          setUser(user);
+        } catch (err) {
+          console.error("Failed to fetch user profile:", err);
+          setUser(null);
+          setToken(null);
+        }
+      }
+    };
 
-    return (
-        <AuthContext.Provider value={{ token, setToken, user, setUser }}>
-            {children}
-        </AuthContext.Provider>
-    );
+    fetchUser();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ token, setToken, user, setUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 export default AuthProvider;
