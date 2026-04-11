bfbf34b63c099f2574ec5515d29a1adeb0d344ea
ig-aditya-101 <adistuding@gmail.com>
2026-04-11 17:29:34 +0530

    feat:finished register page

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState, useEffect } from "react";
+import { useContext, useState, useEffect } from "react";
 import Input from "../utils/Input";
 import Toggle from "../utils/Toggle";
 import Button from "../utils/Button";
