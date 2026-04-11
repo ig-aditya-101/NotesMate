9ff017afd33f6fcd601d588101c64a2200066cb2
ig-aditya-101 <adistuding@gmail.com>
2026-04-11 16:24:28 +0530

    feat:api integration done in Dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,23 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Avatar from "../utils/Avatar";
-import { mockNotes } from "../mock/data";
+
 import NotesCard from "../components/NotesCard";
+import axiosInstance from "../apis/axios";
 
 const Dashboard = () => {
+  const [notes, setNotes] = useState([]);
+  useEffect(() => {
+    const fetchNotes = async () => {
+      try {
+        const res = await axiosInstance.get("/notes");
+        setNotes(res.data.notes);
+      } catch (error) {
+        alert(error);
+      }
+    };
+    fetchNotes();
+  }, []);
   return (
     <div className=" flex flex-col py-2.5 gap-6 px-4 pt-4  overflow-hidden">
       <div className="header flex justify-between items-center">
         <div>
-        <div className="text-display">Hey Adi 👋</div>
-        <div className="text-h3 text-text-secondary p-1">MAIT</div>
+          <div className="text-display">Hey Adi 👋</div>
+          <div className="text-h3 text-text-secondary p-1">MAIT</div>
         </div>
         <div>
-          <Avatar seed="gkjgkkhjiosqasdsa" />
+          <Avatar seed="aditya" />
         </div>
       </div>
       <h1 className="text-h1">My Saved Notes</h1>
       <div className="notesrow flex flex-col items-center   gap-3">
-        {mockNotes.map((note) => (
+        {notes.map((note) => (
           <div className="w-full">
             <NotesCard
               title={note.title}
