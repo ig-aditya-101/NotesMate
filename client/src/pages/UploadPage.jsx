d69acdb625e705926cc040406aaf224fdcbb4a54
ig-aditya-101 <adistuding@gmail.com>
2026-04-11 16:25:06 +0530

    feat: added Upload Page Api logic

--- client/src/pages/UploadPage.jsx
+++ client/src/pages/UploadPage.jsx
@@ -2,18 +2,41 @@ import { UploadCloud } from "lucide-react";
 import React, { useRef, useState } from "react";
 import Input from "../utils/Input";
 import Button from "../utils/Button";
-
+import axiosInstance from "../apis/axios";
+import { useNavigate } from "react-router-dom";
+import Modal from "../utils/Modal";
 const UploadPage = () => {
+  const navigate = useNavigate();
+  const [showModal, setShowModal] = useState(false);
   const fileInputRef = useRef(null);
-  const [title,setTitle]=useState("")
-  const [subject,setsubject]=useState("")
+  const [title, setTitle] = useState("");
+  const [subject, setsubject] = useState("");
+  const [file, setFile] = useState(null);
+
+  const handleUpload = async () => {
+    const uploadBox = new FormData();
+    uploadBox.append("title", title);
+    uploadBox.append("subject", subject);
+    uploadBox.append("file", file);
+    try {
+      await axiosInstance.post("/notes/upload", uploadBox, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+      setShowModal(true);
+    } catch (error) {
+      console.error("Upload failed!");
+    }
+  };
 
   return (
     <div className=" flex flex-col py-2.5 gap-4 px-4 pt-4  overflow-hidden">
-     
-        <div className="text-h1">Upload Notes</div>
-        <div className="text-body text-text-secondary">Share your notes with your college</div>
-      
+      <div className="text-h1">Upload Notes</div>
+      <div className="text-body text-text-secondary">
+        Share your notes with your college
+      </div>
+
       <div
         onClick={() => fileInputRef.current.click()}
         className="border-2 border-dashed border-border-main rounded-xl p-10 text-center cursor-pointer flex flex-col items-center gap-2"
@@ -21,17 +44,47 @@ const UploadPage = () => {
         <UploadCloud />
         <p>Tap to select PDF</p>
         <p className="text-text-muted text-small">{"Max 15MB  ·  PDF only"}</p>
-        <input type="file" ref={fileInputRef} className="hidden"></input>
+        <input
+          type="file"
+          ref={fileInputRef}
+          className="hidden"
+          onChange={(e) => setFile(e.target.files[0])}
+        ></input>
       </div>
       <div className="flex flex-col gap-2 ">
-        <Input placeholder={'Note Title'} value={title} onChange={(e)=>{setTitle(e.target.value)}} />
-        <Input placeholder={'Subject Title'} value={subject} onChange={(e)=>{setsubject(e.target.value)}} />
-        
-        
+        <Input
+          placeholder={"Note Title"}
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+        />
+        <Input
+          placeholder={"Subject Title"}
+          value={subject}
+          onChange={(e) => {
+            setsubject(e.target.value);
+          }}
+        />
       </div>
-      <Button variant="primary" size="lg">Upload Notes</Button>
+      <Button variant="primary" size="lg" onClick={handleUpload}>
+        Upload Notes
+      </Button>
+      <Modal isOpen={showModal} onClose={() => setShowModal(false)}>
+        {/* Everything inside here is the "children" */}
+        <div className="flex flex-col items-center gap-4 p-4 text-center">
+          <h2 className="text-h2 text-success-main">Upload Successful!</h2>
+          <p className="text-body text-text-secondary">
+            Your notes are now live.
+          </p>
+
+          <Button variant="primary" onClick={() => navigate("/dashboard")}>
+            View Dashboard
+          </Button>
+        </div>
+      </Modal>
     </div>
   );
 };
 
-export default UploadPage; 
+export default UploadPage;
