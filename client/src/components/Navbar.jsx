15c0f4e87cc52feac43ce6acd9dbdd2e5865d9be
ig-aditya-101 <adistuding@gmail.com>
2026-04-12 17:53:49 +0530

    feat: implement responsive navigation, dashboard, and browse pages with Cloudinary file upload integration

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,31 +1,59 @@
-import { Circle, Grid, Home, Upload } from 'lucide-react'
-import React from 'react'
+import { Circle, Grid, Home, Upload } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
-    const navbarItems=[
-        {
-            label:'Home',
-            icon:<Home/>
-        },
-        {
-            label:'Browse',
-            icon:<Grid/>
-        },
-        {
-            label:'Upload',
-            icon:<Upload/>
-        },
-        {
-            label:'Profile',
-            icon:<Circle/>
-        }
-
-    ]
+  const navigate = useNavigate();
+  const navbarItems = [
+    {
+      label: "Home",
+      icon: <Home />,
+      route: "/dashboard",
+    },
+    {
+      label: "Browse",
+      icon: <Grid />,
+      route: "/",
+    },
+    {
+      label: "Upload",
+      icon: <Upload />,
+      route: "/upload",
+    },
+    {
+      label: "Profile",
+      icon: <Circle />,
+      route: "/dashboard",
+    },
+  ];
   return (
-    <div className='flex w-full justify-between fixed bottom-0 left-0 right-0 bg-white border-border-main px-4 py-2'>
-       { navbarItems.map((item)=> (<div className='text-text-muted text-micro flex flex-col items-center'>{item.icon}{item.label}</div>))      }
-    </div>
-  )
-}
+    <>
+      <div className="hidden md:flex flex-col fixed left-0 top-0 h-full w-56 bg-bg-secondary border-r border-border-main p-4 gap-2">
+        {navbarItems.map((item) => (
+          <div
+            key={item.label}
+            onClick={() => navigate(item.route)}
+            className="flex items-center gap-3 p-3 rounded-lg cursor-pointer text-text-secondary hover:bg-bg-primary"
+          >
+            {/* {item.icon} */}
+            <span>{item.label}</span>
+          </div>
+        ))}
+      </div>
 
-export default Navbar
+      <div className="flex w-full md:hidden justify-between fixed bottom-0 left-0 right-0 bg-white border-border-main px-4 py-2">
+        {navbarItems.map((item) => (
+          <div
+            key={item.label}
+            className="text-text-secondary text-micro flex flex-col items-center"
+            onClick={() => navigate(item.route)}
+          >
+            {item.icon}
+            {item.label}
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
