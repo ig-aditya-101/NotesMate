717f92b57f7cf63b77328c1781fd6409f33d4d01
ig-aditya-101 <adistuding@gmail.com>
2026-04-20 13:31:24 +0530

    feat: implement dashboard, profile, and registration pages with form validation and API integration

--- client/src/utils/Input.jsx
+++ client/src/utils/Input.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 
-const Input = ({ type, placeholder, value, onChange, error }) => {
+const Input = ({ type, placeholder, value, onChange, name, onBlur, error }) => {
   return (
     <div className="w-full ">
       <input
         className="  border-border-main border-3 bg-bg-primary text-text-primary placeholder:text-text-muted rounded-xl text-body px-4 py-3 outline-none w-full"
         type={type}
+        name={name}
         placeholder={placeholder}
         value={value}
         onChange={onChange}
+        onBlur={onBlur}
       ></input>
       {error && (
         <span className="text-small text-error px-4 italic">{error}</span>
