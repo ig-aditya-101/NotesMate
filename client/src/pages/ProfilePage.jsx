518847de9448bfbfd49f1bb5f6758fab6f3b3dbe
ig-aditya-101 <adistuding@gmail.com>
2026-04-20 13:34:52 +0530

    feat: implement BrowsePage and Dashboard with search and filtering capabilities

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import Avatar from "../utils/Avatar";
+import Badge from "../utils/Badge";
+import StatBox from "../components/StatBox";
+import Button from "../utils/Button";
 
 const ProfilePage = () => {
   return (
-    <div>ProfilePage</div>
-  )
-}
+    <div className="flex flex-col justify-center items-center">
+      <h1 className="">Profile</h1>
+      <div>
+        <Avatar />
+      </div>
+      <div className=" flex justify-center items-center">
+        <Badge>College</Badge>
 
-export default ProfilePage
+        <Badge>Student</Badge>
+      </div>
+      <div className=" flex justify-center items-center">
+        <StatBox label={"Download"} number={"number"} />
+        <StatBox label={"Download"} number={"number"} />
+        <StatBox label={"Download"} number={"number"} />
+      </div>
+      <div>
+        <div>My Uploads</div>
+        <div></div>
+      </div>
+      <div>
+        <Button size="lg">Sign Out</Button>
+      </div>
+    </div>
+  );
+};
+
+export default ProfilePage;
